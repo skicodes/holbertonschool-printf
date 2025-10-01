7ef9ecfad0e18e7e6cc0cea8f08068ec875f7ac7
skicodes <skalrashed02@hotmail.com>
2025-10-01 14:40:13 +0000

    Add %b specifier to print unsigned int in binary

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 int main(void)
 {
-    int len = _printf("Binary: %b\n", 15);
-    _printf("Length printed: %d\n", len);
+    unsigned int u = UINT_MAX;
+    long unsigned int l = (unsigned long)UINT_MAX + 1024;
+    int res = (int)((long)INT_MAX * 2);
+
+    _printf("%b\n", 1024);
+    _printf("%b\n", -1024);
+    _printf("%b\n", 0);
+    _printf("%b\n", u);
+    _printf("%lb\n", l);
+    _printf("There is %b bytes in %b KB\n", 1024, 1);
+    _printf("%b - %b = %b\n", 2048, 1024, 1024);
+    _printf("%b + %b = %lb\n", INT_MAX, INT_MAX, (unsigned long)res);
+
     return 0;
 }
--- main.h
+++ main.h
@@ -6,5 +6,7 @@ int _putchar(char c);
 int print_char(char c);            /* ✅ Add this line */
 int print_string(char *str);
 int print_number(int n);
+int print_binary(unsigned int n);
+int print_binary_long(unsigned long n);
 
 #endif /* MAIN_H */
